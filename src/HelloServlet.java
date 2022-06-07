3f6c5b97731cd496d53466fba1813a4c96ee0ed8
Brock-99 <elmatadordelmar99@gmail.com>
2022-06-07 14:40:38 +0530

    Initial Commitment

---
+++ src/HelloServlet.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by : Naveen Pabhath
+ * Date : 6/7/2022
+ * Time : 11:24 AM
+ * Project Name : javaEE
+ **/
+@WebServlet(urlPatterns = "/Hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Request Resived");
+    }
+}
